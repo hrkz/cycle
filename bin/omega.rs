c611aa0217d122db18159af3ff1d458f6baec625
hrkz <hugo.frezat@gmail.com>
2021-02-21 22:57:53 +0100

    Generate and load the cycle prelude in omega

--- bin/omega.rs
+++ bin/omega.rs
@@ -8,16 +8,18 @@
 /// https://hrkz.github.io/omega/
 use cycle::*;
 
-use lang::Interpreter;
+use lang::{Ast, Interpreter, LangError};
 
 use std::env;
 use std::fs;
 use std::io::{self, stdin, stdout, Write};
 
 fn main() -> io::Result<()> {
-  println!("Hello Cycle! Currently ver. 0/1, or {:?}...", Number::Q(Rational::new(0, 1)).trivial());
+  println!("Cycle 0.1.1 :: Omega, feb 21 2021");
 
   let mut vm = Interpreter::new(1);
+
+  use_prelude(&mut vm).unwrap_or_else(|err| eprintln!("failed to load cycle prelude: {}", err));
   if let Some(filename) = env::args().nth(1) {
     vm.file(filename)
   } else {
@@ -27,13 +29,13 @@ fn main() -> io::Result<()> {
 
 trait Interact {
   fn repl(&mut self) -> io::Result<()>;
-
   fn file(&mut self, filename: String) -> io::Result<()>;
 
-  /// Parse statement
-  /// and run action on resulting expr
-  fn interpret(
-    //.
+  fn run(
+    // parse statement and evaluate
+    // both for
+    // - repl
+    // - file
     &mut self,
     line: usize,
     stmt: &str,
@@ -50,26 +52,23 @@ impl Interact for Interpreter {
       stdout().flush()?;
       stdin().read_line(&mut stmt)?;
 
-      self.interpret(1, &stmt)
+      self.run(1, &stmt)
     }
   }
 
   fn file(&mut self, filename: String) -> io::Result<()> {
     let script = fs::read_to_string(filename)?;
-    script.lines().enumerate().for_each(|(line, stmt)| self.interpret(line + 1, stmt));
+    script.lines().enumerate().for_each(|(line, stmt)| self.run(line + 1, stmt));
 
     Ok(())
   }
 
-  fn interpret(&mut self, line: usize, stmt: &str) {
+  fn run(&mut self, line: usize, stmt: &str) {
     match self.parse(&stmt.trim_end()) {
-      // silent assignment
+      // rule or definition
       Ok(None) => (),
 
-      Ok(
-        //.
-        Some(expr),
-      ) => {
+      Ok(Some(expr)) => {
         let expr = expr.trivial();
         expr.map_or_else(|err| eprintln!("{}", err), |expr| println!("{}", expr))
       }
@@ -83,4 +82,51 @@ impl Interact for Interpreter {
       }
     }
   }
+}
+
+macro_rules!
+gen_prelude {
+  ($vm: ident
+    => $arg0: ident
+      $($Def: tt),*
+      &
+      $($Cte: tt),*
+  ) => {
+    // Generate function definition
+    $( $vm.eval(Ast::Def(
+         Expr::map(stringify!($Def), vec!($arg0.clone())),
+         Expr::$Def($arg0.clone())
+       ))?;
+    )*
+    // Generate constant rules
+    $( $vm.eval(Ast::Rule(
+         Expr::Sym(Symbol::new(stringify!($Cte), Set::SR)),
+         Expr::Cte(Constant::$Cte)
+       ))?;
+    )*
+  }
+}
+
+fn use_prelude(vm: &mut Interpreter) -> Result<(), LangError> {
+  let arg0 = Expr::Sym(Symbol::new("_0", Set::SR));
+
+  gen_prelude!(
+    vm => arg0
+
+    // Trigonometric functions
+    sin, arcsin,
+    cos, arccos,
+    tan, arctan,
+    // Hyperbolic functions
+    sinh, arsinh,
+    cosh, arcosh,
+    tanh, artanh,
+    // Exponential functions
+    exp, log
+    &
+    // Mathematical constants
+    oo, pi, I
+  );
+
+  Ok(())
 }
